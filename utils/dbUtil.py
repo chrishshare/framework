55fee9b55b968ae6e29f8fe17ca967a8a024386d
Administrator <siyzhou@163.com>
2019-12-04 23:22:05 +0800

    统一数据库连接

--- utils/dbUtil.py
+++ utils/dbUtil.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF8 -*-
-import cx_Oracle
 from utils.logUtil import InitLogging
 from utils.yamlParser import YamlParser
-import MySQLdb
-import sqlite3
+import operator
+import jaydebeapi as jdbc
+from getrootdir import root_path
+import os
 
 
 class dbUtil:
@@ -15,98 +16,52 @@ class dbUtil:
         self._yaml = YamlParser().parser_common_conf(cf='projectConfig.yaml')
         self._dbinfo = self._yaml.get(self._yaml.get('project')).get('dbinfo')
         self._dbtype = self._dbinfo.get('type')
-        self._username = self._dbinfo.get('username')
-        self._password = self._dbinfo.get('password')
-        self._database = self._dbinfo.get('database')
-        self._dbfile = self._dbinfo.get('dbfile')
-        self._connstr = self._dbinfo.get('connectstr')
-        self._port = self._dbinfo.get('port')
+        self._username = self._yaml.get(self._dbtype).get('username')
+        self._password = self._yaml.get(self._dbtype).get('password')
+        # self._database = self._dbinfo.get('database')
+        self._jdbcclass = self._yaml.get(self._dbtype).get('jdbcClass')
+        self._jdbcurl = self._yaml.get(self._dbtype).get('jdbcurl')
+        # self._dbfile = self._dbinfo.get('dbfile')
+        # self._connstr = self._dbinfo.get('connectstr')
+        # self._port = self._dbinfo.get('port')
+        # jdbcClass: oracle.jdbc.driver.OracleDriver
+        # jdbcurl: jdbc:oracle: thin:
 
     def connect_database(self):
-        """
-        连接数据库
-        :return:
-        """
-        conn = ''
-        if 'oracle' == self._dbtype:
-            conn = cx_Oracle.connect(self._username, self._password, self._connstr)
-        elif 'mysql' == self._dbtype:
-            conn = MySQLdb.connect(self._connstr, self._username, self._password, self._database)
-        elif 'sqlite3' == self._dbtype:
-            conn = sqlite3.connect(database=self._dbfile)
-        elif 'hive' == self._dbtype:
-            # 暂时没有实现
-            self._logger.error('暂时没有实现')
-            # db = MySQLdb.connect("localhost", "testuser", "test123", "TESTDB", charset='utf8')
-            # cursor = conn.cursor()
+        lib_path = root_path + os.sep + 'libs' + os.sep
+        conn = None
+        cursor = None
+        if operator.eq(self._dbtype, 'oralce'):
+            conn = jdbc.connect(jclassname=self._jdbcclass, jars=lib_path + 'mysql-connector-java-8.0.16.jar',
+                                url=self._jdbcurl, driver_args={'user': self._username, 'password': self._password})
+            cursor = conn.cursor()
+        if operator.eq(self._dbtype, 'mysql'):
+            conn = jdbc.connect(jclassname=self._jdbcclass, url=self._jdbcurl,
+                                driver_args={'user': self._username, 'password': self._password},
+                                jars=lib_path + 'mysql-connector-java-8.0.16.jar')
+            cursor = conn.cursor()
+        if operator.eq(self._dbtype, 'sqlite'):
+            conn = jdbc.connect(jclassname=self._jdbcclass, url=self._jdbcurl, jars=lib_path + 'sqlite-jdbc-3.28.0.jar')
+            cursor = conn.cursor()
+        if operator.eq(self._dbtype, 'hive'):
+            pass
+        if operator.eq(self._dbtype, 'gauss'):
+            pass
         else:
-            self._logger.error('暂不支持该数据库类型')
-        cursor = conn.cursor()
-        return {'cursor': cursor, 'conn': conn}
+            self._logger.error('暂不支持%s数据库类型' % self._dbtype)
+        return {'conn': conn, 'cursor': cursor}
 
-    def query_sql_with_sql(self, sql):
-        """
-        执行不带参数的sql
-        :param sql:
-        :return:
-        """
-        result = self.connect_database().get('cursor').execute(sql)
-        self._close_connect()
-        return result.fetchall()
-
-    def query_sql_with_param(self, sql, param):
-        """
-        执行带参数的sql
-        :param sql:
-        :param param:
-        :return:
-        """
-        result = self.connect_database().get('cursor').execute(sql, param)
-        self._close_connect()
-        return result.fetchall()
-
-    def update_and_insert_sql_with_sql(self, sql):
-        """
-        执行带参数的sql
-        :param sql:
-        :param param:
-        :return:
-        """
-        try:
-            self.connect_database().get('cursor').execute(sql)
-            self.connect_database().get('conn').commit()
-            self._close_connect()
-        except Exception as e:
-            self._logger.error(e)
-
-    def update_and_insert_sql_with_param(self, sql, param):
-        """
-        执行带参数的sql
-        :param sql:
-        :param param:
-        :return:
-        """
-        try:
-            self.connect_database().get('cursor').execute(sql, param)
-            self.connect_database().get('conn').commit()
-            self._close_connect()
-        except Exception as e:
-            self._logger.error(e)
-
-    def _close_connect(self):
-        """
-        关闭连接，查询时需要，插入、更新不需要
-        :return:
-        """
-        self.connect_database().get('cursor').close()
-        self.connect_database().get('conn').close()
+    def execute_single_sql(self, operate, sql, parameters=None):
+        db_obj = self.connect_database()
+        db_obj.get('cursor').execute(operation=sql, parameters=parameters)
+        if operator.eq(operate, 'ddl'):
+            db_obj.get('conn').commit()
+        result = db_obj.get('cursor').fetchall()
+        db_obj.get('cursor').close()
+        db_obj.get('conn').close()
+        return result
 
 
 if __name__ == '__main__':
     sql = """select * from auth_user"""
-    print(dbUtil().query_sql_with_sql(sql=sql))
-
-
-
-
-
+    print(dbUtil().execute_single_sql(sql=sql, operate='dml'))
