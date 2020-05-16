e784708a97ab49de9d75944ef72467e4bf995947
Administrator <siyzhou@163.com>
2020-05-16 18:55:20 +0800

    selenium grid chrome

---
+++ tools/grid/runhub.cmd
@@ -0,0 +1,4 @@
+@echo on
+java -jar selenium-server-standalone-3.141.59.jar -host 192.168.1.7 -role hub -debug
+rem -log log.txt
+@echo off
---
+++ tools/grid/runnode.cmd
@@ -0,0 +1,3 @@
+@echo on
+java  -Dwebdriver.chrome.driver=D:\01_Devspace\03_python\01_projects\02_autotest\framework\drivers\chrome\chromedriver_win32_81.exe -Dwebdriver.firefox.driver=D:\01_Devspace\03_python\01_projects\02_autotest\framework\drivers\firefox\geckodriver-v0.26.0-win64.exe -Dwebdriver.Ie.driver=D:\01_Devspace\03_python\01_projects\02_autotest\framework\drivers\ie\IEDriverServer_x64_3.150.1.exe -jar selenium-server-standalone-3.141.59.jar  -role node -host 192.168.1.7 -hub http://192.168.1.7:4444/
+@echo off
