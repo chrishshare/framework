82dd54a3e247996bd0abae060f740289aba9c06b
Administrator <siyzhou@163.com>
2020-07-11 12:41:29 +0800

    取消多进程

--- execute/runAll.py
+++ execute/runAll.py
@@ -3,8 +3,6 @@ from utils.reportUtil import HTMLTestRunner
 import unittest
 from getrootdir import root_path
 import os
-from multiprocessing import Pool
-from utils.yamlParser import YamlParser
 
 
 if __name__ == '__main__':
@@ -13,10 +11,4 @@ if __name__ == '__main__':
     print(case_count)
     report_path = root_path + os.sep + 'report'
     result = HTMLTestRunner(path=report_path)
-    process_count = int(YamlParser().parser_common_conf().get('threadCount'))
-    po = Pool(process_count)
-    for i, j in zip(test_suite, range(case_count)):
-        po.apply_async(func=result.run, args=(i,))
-
-    po.close()
-    po.join()
+    result.run(test_suite)
